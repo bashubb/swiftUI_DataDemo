41875c94667de1e6124a45a25cca20f58113cc43
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2023-09-05 12:08:45 +0200

    State - Property Wrappers

---
+++ DataSwiftUIDemo/State/PropertyWrappers.swift
@@ -0,0 +1,38 @@
+//
+//  PropertyWrappers.swift
+//  DataSwiftUIDemo
+//
+//  Created by HubertMac on 05/09/2023.
+//
+
+import SwiftUI
+
+struct PropertyWrappers: View {
+    
+    @State var name = ""
+    private let names = ["Mark", "Scott", "Chris", "Sean", "Paul"]
+    
+    var body: some View {
+        VStack {
+            HeaderView("State", subtitle: "Property Wrappers", desc: "Property wrappers can run some logic every time a variable is changed. We can't mutate struct variable values.But @State runs logic that allows variable changes.", back: .blue, textColor: .white)
+            
+            Spacer()
+            Button("Random Name") {
+                name = names.randomElement() ?? ""
+            }
+
+            Text("Name: \(name)")
+            Spacer()
+            
+            DescView(desc: "Modifying a @State variable value does at least two things. 1. Stores our value outside of the struct. 2. Recreates our view without losing the state variable value.", back: .blue, textColor: .white)
+            Spacer()
+        }
+        .font(.title)
+    }
+}
+
+struct PropertyWrappers_Previews: PreviewProvider {
+    static var previews: some View {
+        PropertyWrappers()
+    }
+}
